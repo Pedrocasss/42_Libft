be82d647c835ccdb6339994554794aaa4ad6b9e1
PutoDiKA100 <117018090+PutoDiKA100@users.noreply.github.com>
2022-11-06 20:17:09 +0000

    Add files via upload

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psoares- <psoares-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 12:21:48 by psoares-          #+#    #+#             */
+/*   Updated: 2022/10/31 14:14:19 by psoares-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, '\0', (size * nmemb));
+	return (ptr);
+}
